2a16c999b3867d3286fb289a28ca7455792e1a95
OksanaGreen <Springocsana@ukr.net>
2023-05-22 21:15:00 +0300

    h0me-8-task-1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import myFunc from './path/to/my-func';
+// import MyClass from './path/to/my-class';
+
+// myFunc();
+
+// const inst = new MyClass();
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+let original;
+let preview;
+let description;
+const gallery = document.querySelector('.gallery');
+const galleryMarcup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item np ">
+    <a class="gallery__link" href='${original}' width="800" height="600">
+      <img
+        class="gallery__image"
+        src='${preview}'
+        alt='${description}'
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarcup);
+
+var lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+import Vimeo, { Player } from '@vimeo/player';
+const iframe = document.querySelector(`#vimeo-player`);
+const player = new Vimeo(iframe);
+const STOREGE_KEY = 'videoplayer-current-time';
 
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(STOREGE_KEY, data.seconds);
+  }, 1000)
+);
+// console.log('played the video!');
+const timePause = localStorage.getItem(STOREGE_KEY);
+console.log(timePause);
+if (timePause) {
+  player.setCurrentTime(timePause);
+}
+player.setCurrentTime(localStorage.getItem(STOREGE_KEY) || 0);
+//  const currentTime = JSON.parse(localStorage.getItem(STOREGE_KEY));
+//  if (currentTime !== null) {
+//    player.setCurrentTime(currentTime);
+//  }
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
