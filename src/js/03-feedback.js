69d386802dce8d7e69a34df556e9dbacbb7b23a9
OksanaGreen <Springocsana@ukr.net>
2023-05-26 18:30:40 +0300

    cccccc

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,40 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const LocKey = 'feedback-form';
-checkSave();
+
 form.addEventListener(`input`, throttle(handledInput), 500);
 form.addEventListener(`submit`, onSubmit);
 
 function handledInput(even) {
+  console.log(even);
   even.preventDefault();
-  const {
-    elements: { email, message },
-  } = even.target.value;
+  const { email, message } = even.target.elements;
+  console.dir(even);
+  console.log(email.value);
+  console.log(message.value);
+  LocKey = {
+    Email: email.value,
+    Message: message.value,
+  };
   localStorage.setItem(LocKey, JSON.stringify(elements) || 0);
 }
 function checkSave() {
   const savedSettings = JSON.parse(localStorage.getItem(LocKey));
+
   console.log(savedSettings);
 }
 
 function onSubmit(even) {
   even.preventDefault();
-  if (email.value === '' || message.value === '') {
-    console.log(even.target.value);
-    return;
-  }
-
   even.currentTarget.reset();
   localStorage.removeItem(LocKey);
-  const form = {};
+  if (email.value === '' || message.value === '') {
+    return;
+  }
+  console.log(Email);
+  console.log(Message);
 }
+
 // form.addEventListener('submit', handleSubmit);
 
 // function handleSubmit(event) {
