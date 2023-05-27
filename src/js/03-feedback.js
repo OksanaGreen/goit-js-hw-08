f194197a737cbe71124ca121a4bf0c62b1dfc95f
OksanaGreen <Springocsana@ukr.net>
2023-05-27 17:36:50 +0300

    ffffff

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,37 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const LocKey = 'feedback-form';
+const input = document.querySelector('email');
+const textarea = document.querySelector('message');
 
 form.addEventListener(`input`, throttle(handledInput), 500);
 form.addEventListener(`submit`, onSubmit);
-
+const data = {};
 function handledInput(even) {
-  console.log(even);
   even.preventDefault();
-  const { email, message } = even.target.elements;
-  console.dir(even);
   console.log(email.value);
   console.log(message.value);
-  LocKey = {
-    Email: email.value,
-    Message: message.value,
-  };
-  localStorage.setItem(LocKey, JSON.stringify(elements) || 0);
+
+  data[even.target.name] = even.target.value;
+  localStorage.setItem(LocKey, JSON.stringify(data));
 }
 function checkSave() {
   const savedSettings = JSON.parse(localStorage.getItem(LocKey));
-
+  if (savedSettings) {
+    !savedSettings === null;
+    data.email = input.value;
+    data.message = textarea.value;
+  }
   console.log(savedSettings);
+  return;
 }
-
 function onSubmit(even) {
   even.preventDefault();
   even.currentTarget.reset();
   localStorage.removeItem(LocKey);
-  if (email.value === '' || message.value === '') {
+  if (even.target.value === null) {
     return;
   }
   console.log(Email);
   console.log(Message);
 }
-
-// form.addEventListener('submit', handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { login, password },
-//   } = event.currentTarget;
-
-//   if (login.value === '' || password.value === '') {
-//     return console.log('Please fill in all the fields!');
-//   }
-
-//   console.log(`Login: ${login.value}, Password: ${password.value}`);
-//   event.currentTarget.reset();
